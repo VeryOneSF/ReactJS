e0391e6e97c0639f46f04a7a56a2fbca4bd1a11d
VeryOneSF <gmlwjd789012@gmail.com>
2025-01-30 04:21:19 +0900

    reactJS/Section2/20250130

---
+++ section2/chapter08.js
@@ -0,0 +1,58 @@
+// 5가지 요소 순회 및 탐색 메서드 
+// 1. forEach
+// 모든 요소를 순회하면서 각각각의 요소에 특정 동작을 수행시키는 메서드
+let arr1 = [1, 2, 3];
+
+arr1.forEach(function (item, idx, arr){
+    // console.log(idx, item*2)
+})
+
+let doubledArr = [];
+
+arr1.forEach((item) => {
+    doubledArr.push(item * 2);
+})
+
+// 2. includes
+// 배열에 특정 요소가 있는지 확인하는 메서드
+let arr2 = [1, 2, 3];
+let isInclude = arr2.includes(2)
+
+// 3. indexOf
+// 특정 요소의 인덱스를 찾아서 반환하는 메서드
+// 찾으려는 요소가 없으면 -1 반환
+// 찾으려는 요소가 여러 개라면 가장 먼저 발견되는 요소의 인덱스 반환
+let arr3 = [1, 2, 3];
+let index = arr3.indexOf(2);
+
+let objectArr = [
+    {age : 24}, 
+    {age : 35}
+]
+
+console.log(objectArr.indexOf({age : 24})) // -1 : 얕은 비교 (참조값을 기준으로 비교)
+objectArr.findIndex(
+    (item) => item.age === 24              // 프로퍼티로 비교 가능
+)
+
+// 4. findIndex
+// 모든 요소를 순회하면서, 콜백함수를 만족하는 메서드
+// 특정 요소의 인덱스를 반환하는 메서드
+let arr4 = [1, 2, 3];
+// const findedIndex = arr4.findIndex((item) => {
+//     if(item % 2 !== 0) return true;
+// });
+
+const findedIndex = arr4.findIndex(
+    (item) => {item === 2}
+);
+
+// 5. find
+// 모든 요소를 순회하면서 콜백함수를 만족하는 요소를 찾는데, 요소를 그대로 반환
+let arr5 = [
+    {age : 24}, 
+    {age : 35}
+]
+
+const finded = arr5.find((item) => item.age === 24)
+console.log(finded) // {age : 24}
---
+++ section2/chapter09.js
@@ -0,0 +1,46 @@
+// 5가지 배열 변형 메서드
+// 1. filter
+// 기존 배열에서 조건을 만족하는 요소들만 필터링하여 새로운 배열로 반환
+
+let arr1 = [
+    {name : "홍길동", age : 24},
+    {name : "강길동", age : 24},
+    {name : "임길동", age : 44}
+]
+const people = arr1.filter(
+    (item) => item.age === 24
+)
+
+// 2. map
+// 배열의 모든 요소를 순회하면서, 각각 콜백함수를 실행하고 그 결과값들을 모아서 새로운 배열로 반환
+let arr2 = [1, 2, 3];
+const mapResult1 = arr2.map((item, idx, arr) => {
+    return item * 2;
+});
+
+let names = arr1.map((item) => item.name);
+// console.log(names) // ['홍길동', '강길동', '임길동']
+
+// 3. sort
+// 배열을 사전순을 정렬하는 메서드
+let arr3 = [10, 2, 3, 4];
+arr3.sort((a, b) => {
+    if(a > b) { // b가 a 앞
+        return 1;
+    }else if (b > a) { // a가 b 앞
+        return -1;
+    }else { // a 와 b 자리 그대로 유지
+        return 0;
+    }
+});
+
+// 4. toSorted (가장 최근에 추가된 최신 함수수)
+// 정렬된 새로운 배열을 반환하는 메서드
+let arr5 = ["c", "a", "b"];
+const sorted = arr5.toSorted();
+
+// 5. join
+// 배열의 모든 요소를 하나의 문자열로 합쳐서 반환하는 메서드 
+let arr6 = ["hi", "im", "winterlood"];
+const joined = arr6.join(" ");
+console.log(joined) // hi im winterlood
